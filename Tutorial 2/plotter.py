44d54301448a70b8355c3765bcea3248c8279eb3
Kostis-S-Z <kostis.s.z.24@gmail.com>
2019-02-07 11:52:37 +0100

    Add title option to plotter

--- Tutorial 2/plotter.py
+++ Tutorial 2/plotter.py
@@ -8,10 +8,11 @@ Authors: Kostis SZ, Romina Ariazza and Clara Tump
 from matplotlib import pyplot as plt
 
 
-def plot_2d_function(x, y, y_pred=None):
+def plot_2d_function(x, y, y_pred=None, title=None):
     plt.scatter(x, y, color='blue')
 
     if y_pred is not None:
         plt.scatter(x, y_pred, color='red')
 
+    plt.title(title)
     plt.show()
