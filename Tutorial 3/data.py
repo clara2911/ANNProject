79f0080379f69870a12d248c315d8429355ed2b9
Kostis-S-Z <kostis.s.z.24@gmail.com>
2019-02-11 17:58:41 +0100

    Fix loading data function

--- Tutorial 3/data.py
+++ Tutorial 3/data.py
@@ -14,7 +14,8 @@ def load_file():
     Read file that has one line of integers separated by commas (,)
     :return: a dictionary where values are 2D arrays of 32x32
     """
-    num_of_images = 9
+    num_of_images = 12
+    image_len = 1024
     dim_i_len = 32
     dim_j_len = 32
 
@@ -27,7 +28,7 @@ def load_file():
         image_data = np.zeros((dim_i_len, dim_j_len))
         for dim_i in range(dim_i_len):
             for dim_j in range(dim_j_len):
-                index = (image * dim_i) + dim_j
+                index = (image * image_len) + (dim_i * dim_i_len) + dim_j
                 image_data[dim_i][dim_j] = data[index]
         p[image] = image_data
 
