9473b7a2212c0496a25549cc81c3bac4798fa9b2
Kostis-S-Z <kostis.s.z.24@gmail.com>
2019-02-11 13:11:36 +0100

    Add function to read data from file

---
+++ Tutorial 3/data.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+data.py
+Function to read contents of data file
+
+Authors: Kostis SZ, Romina Arriaza and Clara Tump
+"""
+
+import numpy as np
+
+
+def load_file():
+    """
+    Read file that has one line of integers separated by commas (,)
+    :return: a dictionary where values are 2D arrays of 32x32
+    """
+    num_of_images = 9
+    dim_i_len = 32
+    dim_j_len = 32
+
+    p = {}
+
+    with open('data/pict.dat') as f:
+        data = [int(s) for s in f.readline().split(',')]
+
+    for image in range(num_of_images):
+        image_data = np.zeros((dim_i_len, dim_j_len))
+        for dim_i in range(dim_i_len):
+            for dim_j in range(dim_j_len):
+                index = (image * dim_i) + dim_j
+                image_data[dim_i][dim_j] = data[index]
+        p[image] = image_data
+
+    return p
