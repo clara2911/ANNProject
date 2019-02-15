bc6e00e84de41e677b794193363d07c01bf8f663
clara2911 <clara.tump@hotmail.com>
2019-02-15 13:58:52 +0100

    do assignment 6

--- Tutorial 3/main_3_6.py
+++ Tutorial 3/main_3_6.py
@@ -8,39 +8,57 @@ Authors: Kostis SZ, Romina Ariazza and Clara Tump
 
 import numpy as np
 import itertools
+from collections import defaultdict
 import hopfield_net as hop_net
-from plot import show_trained, show_tested
+from plot import show_trained, show_tested, plot_capacity
 from data import generate_sparse, distort
 
+import time
+
 def main():
-    """ main function"""
-    num_feats = 1024
-    sparseness = 0.1
-    num_samples = [5, 10, 50, 100, 200]
-    distortion = 0.1
-    # which values to test for bias?
-    bias = 0.
-    for num in num_samples:
-        train_set = generate_sparse(num, num_feats, sparseness)
-        test_set = distort(train_set, distortion)
-        accuracy = train_and_test(train_set, test_set, bias, sparseness)
-        print(accuracy)
+    """
+    main function
+    Tests the capacity of the network for recalling exactly the train_set
+    for different values for the bias
+    """
+    num_feats =  256 #512
+    sparseness = 0.1 # 0.1, 0.01
+    num_samples = [2,4,6,8,10,12,14,16,18,20,25,30]
+    bias_list = [-1,-0.5,-0.1,0,0.1,0.5,1]
+    num_dict = defaultdict(list)
+    acc_dict = defaultdict(list)
+    for bias in bias_list:
+        print("bias: ", bias)
+        for num in num_samples:
+            print("num: ", num)
+            train_set = generate_sparse(num, num_feats, sparseness)
+            # test_set = distort(train_set, distortion)
+            test_set = train_set
+            recalled_set = train_and_test(train_set, test_set, bias, sparseness)
+            accuracy = report_acc(recalled_set, train_set)
+
+            num_dict[bias].append(num)
+            acc_dict[bias].append(accuracy)
+
+    plot_capacity(num_dict, acc_dict, bias_list, sparseness, num_feats)
 
 
 def train_and_test(train_set, test_set, bias, sparseness):
     hop = hop_net.HopfieldNet(train_set)
     hop.sparse_train(sparseness)
-    # show_trained(train_set, 32, 32)
-
     # batch or sequential
-    # recalled_set = hop.recall_01s(test_set, epochs=15, threshold=bias)
-    recalled_set = hop.sequential_recall_01s(test_set, epochs=10, threshold=bias)
+    # recalled_set = hop.recall_01s(test_set, epochs=100, threshold=bias)
+    recalled_set = hop.sequential_recall_01s(test_set, epochs=5000, threshold=bias)
+    return recalled_set
+
+def report_acc(recalled_set, train_set):
     correct = 0
     for recalled_item in recalled_set:
         for trained_item in train_set:
             if np.array_equal(trained_item, recalled_item):
                 correct += 1
-    acc =  correct / test_set.shape[0]
+                break
+    acc =  correct / recalled_set.shape[0]
     return acc
 
 
