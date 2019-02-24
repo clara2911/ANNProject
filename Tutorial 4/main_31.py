626528c274070d2d3f9475112daac56dff11ecee
clara2911 <clara.tump@hotmail.com>
2019-02-24 20:30:11 +0100

    make reporting functions mainly plots

--- Tutorial 4/main_31.py
+++ Tutorial 4/main_31.py
@@ -1,13 +1,14 @@
-import numpy as np
-from matplotlib import pyplot as plt
+#!/usr/bin/env python
+"""
+Main file for Tutorial 4 Part 3.1
+Uses an auto-encoder (Autoencoder class) for different experiments
 
-import keras
-from keras.layers import Input, Dense, Dropout, Activation, Flatten, Convolution2D, MaxPooling2D
-from keras import optimizers
-from keras.models import Model, Sequential
-from keras.utils import np_utils
-from keras.initializers import RandomNormal
+Authors: Kostis SZ, Romina Ariazza and Clara Tump
+"""
+
+import numpy as np
 import data
+import plot
 from auto_encoder import Autoencoder
 
 np.random.seed(10)
@@ -16,12 +17,23 @@ def main():
     x_train, y_train, x_test, y_test = data.mnist()
 
     params = {
-            "epochs" : 5
+            "epochs" : 2,
+            "num_hid_nodes": 32,
         }
     auto_enc1 = Autoencoder(**params)
-    auto_enc1.train(x_train, x_test)
+    history = auto_enc1.train(x_train, x_test)
+    # plot.plot_loss(history, loss_type='MSE')
+    x_reconstr = auto_enc1.test(x_test, binary=True)
+    plot_traintest(x_test, y_test, x_reconstr)
 
-
+def plot_traintest(x_test, y_test, x_reconstr):
+    true_ims_plot, reconstr_ims_plot = np.zeros([10, 784]), np.zeros([10, 784])
+    for i in range(10):
+        indices, = np.where(y_test == i)
+        index = indices[0]
+        true_ims_plot[i] = x_test[index]
+        reconstr_ims_plot[i] = x_reconstr[index]
+    plot.plot_images(true_ims_plot, reconstr_ims_plot)
 
 if __name__ == "__main__":
     main()
--- Tutorial 4/notes.py
+++ Tutorial 4/notes.py
@@ -23,69 +23,58 @@ Setup DNN
 # Output layer dims = num of classes for 3.2
 
 # Train using gradient descent loss
+
 # Clara: all of the above is done and can be found in autoencoder.py
 
+# Report performance using:
+# - MSE: For each image compute the MSE of original input and reconstructed input
+# - Testing: sample one image from each class and plot side-by-side original and reconstructed
+# Clara: Done and can be found in plot.py --> plot_loss and plot_images
+
 """
-Experiments for 3.1: Shallow Autoencoder
+Experiments for 3.1: Single-layer Autoencoder
 """
 
-"""1"""
+"""1: Learning rate"""
 # Experiment with learning rate
 # 1) Experiment with different learning rates and plot
 # 2) Add learning rate decay oer epochs (parameter in optimizer)
 
-"""0"""
+"""2: ReLU"""
 # study ReLU activations in encoder layer
 # Remember: Output layer should always use sigmoid (output range is [0, 1])
 # comment on how they compare with the sigmoidal units, especially
 # in terms of the convergence rate (particularly interesting for the overcomplete
 # autoencoders).
 
-"""2"""
+"""3: hidden layer size"""
 # Test different number of hidden units
 # h_u < input dims (undercomplete)
 # h_u = input dims
 # h_u > input dims (overcomplete)
 # for the overcomplete case use a regularizer (L1 or L2) during training
+# compare image result for different sizes: use plot_images
 
-"""3"""
-# Test how ReLU and sigmoid compare in the hidden layers in terms of convergence rate
-# (especially for overcomplete autoencoders)
+"""3b: Sparseness"""
+# compare the average sparseness of the hidden layer for different number of hidden nodes
+# assume some low threshold below which the absolute value of the unit activation is neglected
+# you can use np.where(l < thresh, 0,1) for this
+# average sparseness of layer = zero_units / number of units in layer
+# discuss how the higher degree of sparseness can be promoted in hidden layer representations
 
-"""4"""
+"""4: Denoising auto-encoders"""
 # ONLY for the undercomplete autoencoders employ denoising autoencoders meaning
 # the target outputs are the original input samples with additive gaussian noise
 # y = x + gaussian noise (test with m=0.  s=0.1-0.3)
 
-
-"""5"""
-# For each image compute the MSE of original input and reconstructed input
-# Save each MSE and at the end of epoch get the mean and std to calculate the epoch's MSE
-# At end of training plot each epoch's mean and std MSE
-
-"""6"""
-# Testing: sample one image from each class and plot side-by-side original and reconstructed
-
-"""7"""
-# Try different sizes of the hidden layer representation and compare errors.
-
-"""8"""
-# Get a trained NN and for each layer measure the average sparseness.
-# For each layer:  (this can be done with np.where(units < 0.1, 0, 1)
-#   For each unit:
-#       if unit is below threshold (~0.1?)
-#           dead_units += 1
-#   average sparseness of layer = dead_units / number of units in layer
-# "discuss how the higher degree of sparseness can be promoted in hidden layer representations"
-
-"""9"""
+"""5: Plot node weights"""
 # Plot weights of units
 # Reshape weight vector to matrix
 # Plot for 100, 200, 400 hidden nodes
 
 
 """
-Experiments for 3.2: Classification using a deep pre-trained autoencoder
+Experiments for 3.2: Classification using a stacked pre-trained autoencoder
 
 The point of this part is to use a Deep Neural Network with the hidden layers pretrained as an Autoencoder
 Then use this representation to the final output layer for classification
